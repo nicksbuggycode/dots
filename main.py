37280b70fd6cd881f0e68127a6411257a442dfd6
bzzz <nweather2@gmail.com>
2022-10-28 08:18:27 -0400

    zips instead of txts

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import csv
 from io import StringIO
+from zipfile import ZipFile
 
 st.header("Batch Dot Phrase Wizard")
 
@@ -16,9 +17,16 @@ if b is not None:
     string_data = stringio.read()
     splitdata = string_data.split()
     st.write(splitdata)
+    zipObj = ZipFile("dotfiles.zip", "w")
 
     for i in splitdata[1:]:
-        text_contents = i
-        st.download_button(f"{i.split()[0]}", text_contents, f"{i.split()[0]}.txt" )
+        #text_contents = i
+        #st.download_button(f"{i.split()[0]}", text_contents, f"{i.split()[0]}.txt" )
+        fname = f"{i.split()[0]}.txt"
+        with open(fname, "w") as txtfile:
+            zipObj.write(txtfile)
+
+    zipObj.close()
+    st.download_button("Download your Zipfile", zipObj, "dotfiles.zip")
 
 st.write("Enjoy!")
