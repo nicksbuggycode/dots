c774f2cdf651f1a40fe121cfba6b46bc0dc2abd0
bzzz <nweather2@gmail.com>
2022-11-08 09:50:54 -0500

    legalizing file name

--- main.py
+++ main.py
@@ -4,31 +4,53 @@ from io import StringIO
 from zipfile import ZipFile
 from io import BytesIO
 
+
+def legalizer(s: str) -> str:
+    dct = {}
+    dct["<"] = "{less}"
+    dct[">"] = "{greater}"
+    dct[":"] = "{colon}"
+    dct['""'] = "{quote}"
+    dct["/"] = "{fslash}"
+    dct["\\"] = "{bslash}"
+    dct["|"] = "{pipe}"
+    dct["?"] = "{qstmrk}"
+    dct["*"] = "{star}"
+    dct[";"] = "{semi}"
+    for i in s:
+        try:
+            dct[i]
+            s = s.replace(i, dct[i])
+            s = s.replace("\n", "")
+            s = s.replace("\t", "")
+        except:
+            continue
+    return s.strip()
+
+
 st.header("Batch Dot Phrase Wizard")
 
 b = st.file_uploader("Upload a csv file")
 
 if b is not None:
     bytes_data = b.getvalue()
-   # To convert to a string based IO:
+    # To convert to a string based IO:
     stringio = StringIO(b.getvalue().decode("utf-8"))
 
-    #To read file as string:
+    # To read file as string:
     st.write("The contents of your csv file are displayed below:")
     string_data = stringio.read()
     splitdata = string_data.split()
     st.write(splitdata)
     with BytesIO() as buffer:
-    # Write the zip file to the buffer
+        # Write the zip file to the buffer
         with ZipFile(buffer, "w") as zip:
             for i in splitdata[1:]:
-                name = f"{i.split()[0]}.txt"
+                name = f"{legalizer(i)}.tx"
                 zip.writestr(name, "file contents")
 
         btn = st.download_button(
-        label="Download ZIP",
-        data=buffer,  # Download buffer
-        file_name="file.zip"
+            label="Download ZIP", data=buffer, file_name="file.zip"  # Download buffer
         )
 
 st.write("Enjoy!")
