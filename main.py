871dd64b4f588d491a8d0f7594c27750913a33ff
bzzz <nweather2@gmail.com>
2022-11-08 11:10:29 -0500

    split the splitdata?

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ if b is not None:
     st.write("The contents of your csv file are displayed below:")
     string_data = stringio.read()
     splitdata = [i for i in string_data.split("\n") if len(i) > 0]
-    st.table(splitdata)
+    st.table(splitdata.split(","))
     st.write("all data written")
     with BytesIO() as buffer:
         # Write the zip file to the buffer
