76080cb91c549464c9631d4493c9ca3cc2778bbf
bzzz <nweather2@gmail.com>
2022-11-09 09:35:57 -0500

    stringdata

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ if b is not None:
     
     st.write("CSV parsing try")
     #parse csv
-    reader = csv.reader(stringio, delimiter=",")
+    reader = csv.reader(string_data, delimiter=",")
     for row in reader:
         print(row)
         st.write(row)
