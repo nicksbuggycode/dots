cbf49d6c43e3f9f490078062f5efe9aba17a4534
bzzz <nweather2@gmail.com>
2022-11-09 09:41:50 -0500

    discard split column

--- main.py
+++ main.py
@@ -44,8 +44,8 @@ if b is not None:
             for i in splitdata[1:]:
                 name = f"{legalizer(i).strip()}"
                 try:
-                    Ldesc, Ldotphrase, Lfulltxt, Lcat, Lauthor = name.split(",")
-                    desc, dotphrase, fulltxt, cat, author = i.split(",")
+                    Ldesc, Ldotphrase, Lfulltxt, Lcat, Lauthor, discard = name.split(",")
+                    desc, dotphrase, fulltxt, cat, author, discard = i.split(",")
                     contents = f"{desc}\n{dotphrase}\n{fulltxt}\n{cat}\n{author}\n{20220415102629}"
                     dotFilename = f"Desc = {Ldesc}; Dotphrase = {Ldotphrase}; Fulltext = {Lfulltxt}; Cat = {Lcat}; Authr = {Lauthor}.bstr"
                     st.write(dotFilename)
